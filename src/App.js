0ee976e07c6dcbb8d367a3794f353f867b505b19
Feixuan Li <fli@swensonhe.com>
2018-08-18 20:42:29 -0700

    firebase setuo

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';//destrucure
 
 class App extends Component {
+    componentWillMount() {
+        firebase.initializeApp({
+            apiKey: 'AIzaSyAmgj5p3mI4qX8HkbKAUv2mOE4sgZVJhiw',
+            authDomain: 'auth-71ecc.firebaseapp.com',
+            databaseURL: 'https://auth-71ecc.firebaseio.com',
+            projectId: 'auth-71ecc',
+            storageBucket: 'auth-71ecc.appspot.com',
+            messagingSenderId: '127285889767'
+          });
+    }
+
     render() {
         return ( 
             <View>
